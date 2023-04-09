53e232c3e0529947deca261ced7106a6d57aa91b
Mario Sotelo <msotelo@whitestack.com>
2023-04-09 00:58:36 -0500

    Adding first run screen

---
+++ lib/screens/first_run_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/colors/gf_color.dart';
+import 'package:getwidget/components/image/gf_image_overlay.dart';
+import 'package:getwidget/components/intro_screen/gf_intro_screen.dart';
+import 'package:getwidget/components/intro_screen/gf_intro_screen_bottom_navigation_bar.dart';
+
+class FirstRunScreen extends StatefulWidget {
+  const FirstRunScreen({super.key});
+
+  @override
+  State<FirstRunScreen> createState() => _FirstRunScreenState();
+}
+
+class _FirstRunScreenState extends State<FirstRunScreen> {
+  late PageController _pageController;
+  late List<Widget> slideList;
+  late int initialPage;
+
+  @override
+  void initState() {
+    _pageController = PageController();
+    initialPage = _pageController.initialPage;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: GFIntroScreen(
+        color: Colors.blueGrey,
+        slides: slides(),
+        pageController: _pageController,
+        currentIndex: 0,
+        pageCount: 5,
+        introScreenBottomNavigationBar: GFIntroScreenBottomNavigationBar(
+          pageController: _pageController,
+          pageCount: slideList.length,
+          currentIndex: initialPage,
+          onForwardButtonTap: () {
+            _pageController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear);
+          },
+          onBackButtonTap: () {
+            _pageController.previousPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear);
+          },
+          navigationBarColor: Colors.white,
+          showDivider: false,
+          inactiveColor: Colors.grey.shade200,
+          activeColor: GFColors.SUCCESS,
+
+        ),
+      ),
+    );
+  }
+
+  List<Widget> slides() {
+    slideList = [
+      Container(
+        child: GFImageOverlay(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(16),
+          color: Colors.orange,
+          image: const AssetImage('assets/images/1001tickets.png'),
+          boxFit: BoxFit.contain,
+          borderRadius: BorderRadius.circular(5),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 70.0, left: 20),
+                child: Text(
+                  'Welcome!',
+                  style: TextStyle(
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      fontSize: 25),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(0),
+            image: const DecorationImage(
+              image: AssetImage('assets/images/1001tickets.png'),
+              fit: BoxFit.contain,
+              
+            )),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(0),
+            image: const DecorationImage(
+              image: AssetImage('assets/images/1001tickets.png'),
+              fit: BoxFit.contain,
+              
+            )),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(0),
+            image: const DecorationImage(
+              image: AssetImage('assets/images/1001tickets.png'),
+              fit: BoxFit.contain,
+              
+            )),
+      ),
+    ];
+    return slideList;
+  }
+}
