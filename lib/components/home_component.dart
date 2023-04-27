630ff4f972da9ba941ddb7cef86c22c8170f0922
Mario Sotelo <msotelo@whitestack.com>
2023-04-27 15:20:13 -0500

    updating home component

--- lib/components/home_component.dart
+++ lib/components/home_component.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter1001tickets/utils/my_styles.dart';
+import 'package:gap/gap.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 class HomeComponent extends StatelessWidget {
   const HomeComponent({super.key});
@@ -8,11 +11,11 @@ class HomeComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[100],
-        body: Padding(
+        body: Container(
           padding: MyStyles.myDefaultHorizontalAppPadding,
           child: ListView(
             children: [
-              Padding(
+              Container(
                 padding: MyStyles.myDefaultVerticalElementsPadding,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -24,33 +27,68 @@ class HomeComponent extends StatelessWidget {
                   ],
                 ),
               ),
-              Padding(
-                padding: MyStyles.myDefaultVerticalElementsPadding,
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          // DateFormat('EEEE d ' 'de' ' MMMM', 'es').format(DateTime.now()),
-                          "Lunes 3 de Marzo".toUpperCase(),
-                          style: MyStyles.supportingTextStyle,
-                        ),
-                        // const SizedBox(height: 10.0),
-                        Text(
-                          // _getGreeting(),
-                          "Buenas tardes, Mario",
-                          style: MyStyles.h1Style,
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              
+              const Gap(20),
+              const GreetingWidget(),
             ],
           ),
         ));
+  }
+}
+
+class GreetingWidget extends StatefulWidget {
+  const GreetingWidget({
+    super.key,
+  });
+
+  @override
+  State<GreetingWidget> createState() => _GreetingWidgetState();
+}
+
+class _GreetingWidgetState extends State<GreetingWidget> {
+  String fdate = "";
+  String fgreet = "";
+
+  @override
+  Widget build(BuildContext context) {
+    initializeDateFormatting('es', null).then((_) {
+      var now = DateTime.now();
+      var formatter = DateFormat("EEEE, d' de 'MMMM", 'es');
+      var greeting = "Buenos días";
+      if (now.hour < 12) {
+        greeting = 'Buenos días';
+      } else if (now.hour < 18) {
+        greeting = 'Buenas tardes';
+      } else {
+        greeting = 'Buenas noches';
+      }
+      setState(() {
+        fdate = formatter.format(now);
+        fgreet = greeting;
+      });
+    });
+    return Container(
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                // DateFormat('EEEE d ' 'de' ' MMMM', 'es').format(DateTime.now()),
+                fdate.toUpperCase(),
+                style: MyStyles.supportingTextStyle,
+              ),
+              // const SizedBox(height: 10.0),
+              Text(
+                // _getGreeting(),
+                fgreet,
+                // TO DO: when firebase is fully connected, it should be a better idea to say something like "buenas tardes, XXXX"
+                style: MyStyles.h1Style,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
